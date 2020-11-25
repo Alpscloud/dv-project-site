0e59de0b6d2c45b2b29ad8e44a3a0462759f2769
winde <winde100kg@gmail.com>
2020-11-25 15:28:36 +0200

    added services pages

--- src/js/common.js
+++ src/js/common.js
@@ -82,6 +82,12 @@ $(document).ready(function() {
 		$('html').addClass('is-fixed');
 	});
 
+	$('.js-open-service-popup-btn').on('click',function(e) {
+		e.preventDefault();
+		$('.js-popup-service').fadeIn(300);
+		$('html').addClass('is-fixed');
+	});
+
 
 	$('.js-close-popup-btn').on('click',function(e) {
 		e.preventDefault();
@@ -249,47 +255,118 @@ $(document).ready(function() {
 	});
 
 	// ========= Ajax form ===========
-	$('.js-input').on('focus',function() {
-		if($(this).hasClass('is-error')) {
-			$(this).removeClass('is-error');
-		}
-	});
+    $('.js-input').on('focus', function () {
+        if ($(this).hasClass('is-error')) {
+            $(this).removeClass('is-error');
+        }
+    });
 
 
- $('.js-form').on('submit', function(e) {
-		e.preventDefault();
+    var send_form_flag = false; /* false resend form while loading */
+    $('.js-form').on('submit', function (e) {
+        e.preventDefault();
 
-		var self = $(this);
+        var self = $(this);
 
-		var inputs = self.find('.js-input'),
-		flag = true;
+        self.find('.form-send-error-text').hide();
 
-		var formData = new FormData(self.get(0));
+        var inputs = self.find('.js-input'),
+            flag = true;
 
-		// Validate
-		$(inputs).each(function() {
-			if(!$(this).val() || $(this).val() == "") {
-				$(this).addClass('is-error');
-				flag = false;
-			}
-		});
+        var formData = new FormData(self.get(0));
 
-		if(!flag) {return false;}
+        // Validate
+        $(inputs).each(function () {
+            if (!$(this).val() || $(this).val() == "") {
+                $(this).addClass('is-error');
+                flag = false;
+            }
+        });
+		
+		var error_div = self.find('.form-send-error-text');
+		if (error_div.length == 0) {
+            self.find('.btn[type="submit"]').after('<div class="form-send-error-text" style="display:none;"></div>');
+            error_div = self.find('.form-send-error-text');
+        }
 
-		$.ajax({
-			contentType: false, 
-      processData: false, 
-      type: "POST",
-			url: "/wp-content/themes/dv-project-theme/contacts.php", //Change
-			data: formData
-		}).done(function() {
-			self.trigger("reset");
-			self.find(".input__file--name").removeClass('is-active');
-			window.location.href ='/typage';
-		});
+		var VALIDATE_FILE_EXTENSIONS = false;
+		var VALIDATE_FILE_SIZE = true;
+		var TRUE_ALL_FILE_SIZE = 10000000;
+        var TRUE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'doc', 'xls', 'ppt', 'pps', 'docm', 'docx', 'xlsx', 'xlsm', 'xlsb', 'pptx', 'pptm', 'ppsx', 'ppsm', 'pdf', 'rar', 'zip', '7z', 'tar', 'tar.gz', 'gz', 'gzip', 'tar-gz', 'dwg'];
+        try {
+			var file_list = formData.getAll("fileFF[]");
+            	var no_valid_files = [];
+            	var all_files_size = 0;
+            	for (var i = 0, file; file = file_list[i]; i++) {
+            	    var sFileName = file.name;
+            	    var sFileExtension = sFileName.split('.')[sFileName.split('.').length - 1].toLowerCase();
+            	    all_files_size += file.size;
+            	    if (VALIDATE_FILE_EXTENSIONS === true && !(TRUE_EXTENSIONS.includes(sFileExtension))) {
+            	        no_valid_files.push(sFileName);
+            	    }
+            	}
+            	if (no_valid_files.length > 0 && VALIDATE_FILE_EXTENSIONS === true) {
+            	    flag = false;
+					self.find('.input__file[name="fileFF[]"]').val('');
+					self.find('.input__file--name').html('');
+            	    error_div = self.find('.form-send-error-text');
+            	    error_div.html('Извините, письмо не отправлено. <br>Невозможно загрузить некоторые файлы: ' + no_valid_files.join('<br>'));
+            	    error_div.show();
+            	    return false;
+            	}
+            	if (all_files_size > TRUE_ALL_FILE_SIZE && VALIDATE_FILE_SIZE === true) {
+					flag = false;
+					self.find('.input__file[name="fileFF[]"]').val('');
+					self.find('.input__file--name').html('');
+					error_div = self.find('.form-send-error-text');
+            	    error_div.html('Письмо не может быть отправлено, так как файл весит больше 10мб, вышлете его на почту <a href="mailto:aps@dv-p.ru">aps@dv-p.ru</a>');
+            	    error_div.show();
+            	    return false;
+            	}
+        } catch (exception) {
+			
+        }
 
-	});
-	// ========= =========== =========== ===========
+        if (!flag) {
+            return false;
+        }
+        if (send_form_flag) return false;
+
+        var sendBtn = self.find('.btn[type="submit"] .btn__text');
+        var savedBtnText = sendBtn.html();
+        sendBtn.html('Идет отправка формы');
+        send_form_flag = true;
+        $.ajax({
+            contentType: false,
+            processData: false,
+            type: "POST",
+            url: "/wp-content/themes/dv-project-theme/contacts.php", //Change
+            data: formData,
+            success: function (data) {
+                sendBtn.html(savedBtnText);
+                send_form_flag = false;
+                error_div = self.find('.form-send-error-text');
+                if (data == "OK") {
+                    self.trigger("reset");
+                    self.find(".input__file--name").removeClass('is-active');
+                    error_div.hide();
+                    window.location.href = '/typage';
+                } else {
+                    error_div.html(data);
+                    error_div.show();
+                }
+            },
+            error: function (resp) {
+                sendBtn.html(savedBtnText);
+                send_form_flag = false;
+
+                error_div = self.find('.form-send-error-text');
+                error_div.html('Ошибка отправки формы');
+                error_div.show();
+            }
+        });
+    });
+    // ========= =========== =========== ===========
 
 
 	$('.js-input-phone').mask("+7(999)-999-99-99",{placeholder:" "});
